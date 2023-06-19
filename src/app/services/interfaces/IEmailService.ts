d1f6f5e2b69b0e0f6ae6577be9c5f0a715f6d75e
GhabryelHenrique <ghabryelhenrique@hotmail.com>
2023-06-19 13:18:05 -0300

    Fix bug

--- src/app/services/interfaces/IEmailService.ts
+++
@@ -1,6 +0,0 @@
-import { EmailPayload } from "../../models/email";
-
-export interface IEmailService {
-  getEmailbyID(emailId: string): Promise<EmailPayload>;
-  sendEmail(emailPayload: EmailPayload): Promise<void>;
-}
